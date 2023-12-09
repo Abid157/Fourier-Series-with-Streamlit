4ec227fb52b7b7421131d000c5b115e46bfad350
Abid Hasan <127683594+Abid157@users.noreply.github.com>
2023-12-09 23:38:41 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter, MultipleLocator
+
+# Streamlit App
+import streamlit as st
+st.title("Fourier Series Plotter")
+h = st.number_input("Number of Harmonics", min_value=1, max_value=100, value=10, step=1)
+
+# Generate x values
+N = 1000
+x = np.linspace(-np.pi, np.pi, N)
+
+# Define the function to be approximated with its Fourier series
+original_function = lambda x: np.array([1 if 2 * abs(i) < np.pi else -1 for i in x])
+
+# Fourier coefficients Definition
+a0 = 0
+a = lambda n: 4 * (1j ** n).imag / (n * np.pi)
+b = lambda n: 0
+coefficients = [a0] + [(a(n), b(n)) for n in range(1, h + 1)]
+
+# Calculate Fourier series coefficients for the given function
+def fourier_coefficients(func, n_terms=10):
+    a0 = np.trapz(func, dx=2 * np.pi / N) / (2 * np.pi)
+    coefficients = [a0]
+    for n in range(1, n_terms + 1):
+        an = np.trapz(func * np.cos(n * x), dx=2 * np.pi / N) / np.pi
+        bn = np.trapz(func * np.sin(n * x), dx=2 * np.pi / N) / np.pi
+        coefficients.append((an, bn))
+    return coefficients
+
+# coefficients = fourier_coefficients(original_function(x), n_terms=h)
+
+# Reconstruct the function using its Fourier series expansion
+def fourier_series(coefficients, x):
+    series = coefficients[0]
+    for n, (an, bn) in enumerate(coefficients[1:], start=1):
+        series += an * np.cos(n * x) + bn * np.sin(n * x)
+    return series
+
+reconstructed_function = fourier_series(coefficients, x)
+
+# Create a single figure and plot both the original function and Fourier series approximation
+fig, ax = plt.subplots(figsize=(10, 5))
+
+# Plot the original function
+ax.plot(x, original_function(x), label='Original Function', color='blue')
+
+# Plot the Fourier series approximation on the same axes
+ax.plot(x, reconstructed_function, label=f'Fourier Series with {h} harmonics', color='red')
+
+ax.set_xlabel('$\omega_1$t')
+ax.set_ylabel('f($\omega_1$t)')
+ax.axhline(0, color='black', linewidth=0.5)
+ax.axvline(0, color='black', linewidth=0.5)
+ax.xaxis.set_major_formatter(
+    FuncFormatter(lambda val, pos: f'${val / np.pi: .1f}\pi$' if val != 0 else '0')
+)
+ax.xaxis.set_major_locator(MultipleLocator(base=np.pi / 2))
+ax.xaxis.set_major_locator(MultipleLocator(base=np.pi / 2))
+ax.legend()
+
+st.pyplot(fig)
+
+from sympy import fourier_series, pi, plot
+from sympy.abc import x
+f = x
+s = fourier_series(f, (x, -pi, pi))
+s1 = s.truncate(n = 3)
+s2 = s.truncate(n = 5)
+s3 = s.truncate(n = 7)
+p = plot(f, s1, s2, s3, (x, -pi, pi), show=False, legend=True)
+
+p[0].line_color = 'red'
+p[0].label = 'x'
+p[1].line_color = 'green'
+p[1].label = 'n=3'
+p[2].line_color = 'blue'
+p[2].label = 'n=5'
+p[3].line_color = 'black'
+p[3].label = 'n=7'
+
+p.show()
+st.pyplot(p._backend.fig)
